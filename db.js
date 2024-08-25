cf4eed97bbce64b3d8e679e7d3cf6b7ac1e064da
Inkireon <ink.on.the.papyrus@gmail.com>
2024-08-26 02:24:35 +0500

    Update db.js

--- db.js
+++ db.js
@@ -2,11 +2,7 @@ import pkg from 'pg';
 const { Pool } = pkg;
  
 const pool = new Pool({
-    user: "postgres",
-    password: "123456",
-    host: "localhost",
-    port: 5432,
-    database: "adl"
+    connectionString: process.env.POSTGRES_URL,
 })
  
 export const query = (text, params) => pool.query(text, params);
