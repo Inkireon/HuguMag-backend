0d63667f55b4cb1303752c326f0560ffb2241f17
Inkireon <ink.on.the.papyrus@gmail.com>
2024-08-26 21:23:03 +0500

    Update ArticleController.js

--- controllers/ArticleController.js
+++ controllers/ArticleController.js
@@ -1,13 +1,13 @@
-import {query} from "../db.js"
+import { query } from "../db.js"
 
 
 
 
 async function get(req, res) {
-    
+
     try {
         const { section } = req.query;
-        const data = await query('SELECT * FROM articles WHERE section = $1',[section]);
+        const data = await query('SELECT * FROM articles WHERE section = $1', [section]);
         res.json(data.rows);
     } catch (error) {
         console.error('Error querying database:', error);
@@ -16,12 +16,12 @@ async function get(req, res) {
 };
 
 async function post(req, res) {
-    console.log("req.body",req.body)
-    console.log("req.file",req.file) 
+    console.log("req.body", req.body)
+    console.log("req.file", req.file)
 
     try {
-        const { name, shortInfo} = req.body;
-        const articleImage = req.file ? req.file.path : null; 
+        const { name, shortInfo } = req.body;
+        const articleImage = req.file ? req.file.path : null;
         await query('INSERT INTO articles (name, shortinfo, articleimage) VALUES ($1, $2, $3)', [name, shortInfo, articleImage]);
         res.status(200).send({ message: "succsess !!!" });
     } catch (error) {
@@ -32,44 +32,45 @@ async function post(req, res) {
 };
 
 async function put(req, res) {
-    console.log("req.body",req.body)
-    console.log("req.file",req.file) 
+    console.log("req.body", req.body)
+    console.log("req.file", req.file)
     try {
-        const { ID,title, paragraph, photo, hide, section, size , placement} = req.body;
+        const { ID, title, paragraph, photo, hide, section, size, placement } = req.body;
         const articleImage = req.file ? req.file.path : null;
-    
-            switch (section) {
-                case 'social':
-                    await query('UPDATE articles SET title = $1, paragraph = $2, photo = $3, hide = $4, section = $5 WHERE ID = $6', [title, paragraph, photo, hide, section, ID]);
-                    break;
-        
-                case 'beauty':
-                    await query('UPDATE articles SET title = $1, paragraph = $2, photo = $3, hide = $4, section = $5, placement = $6 WHERE ID = $7', [title, paragraph, photo, hide, section, placement, ID]);
-                    break;
-        
-                case 'art':
-                    await query('UPDATE articles SET title = $1, paragraph = $2, photo = $3, hide = $4, section = $5 WHERE ID = $6', [title, paragraph, photo, hide, section, ID]);
-                    break;
 
-                case 'events':
-                    await query('UPDATE articles SET title = $1, paragraph = $2, photo = $3, hide = $4, section = $5 WHERE ID = $6', [title, paragraph, photo, hide, section, ID]);
-                    break;
+        switch (section) {
+            case 'social':
+                await query('UPDATE articles SET title = $1, paragraph = $2, photo = $3, hide = $4, section = $5 WHERE ID = $6', [title, paragraph, photo, hide, section, ID]);
+                break;
 
-                case 'top-news':
-                    await query('UPDATE articles SET title = $1, paragraph = $2, photo = $3, hide = $4, section = $5 WHERE ID = $6', [title, paragraph, photo, hide, section, ID]);
-                    break;
-        
-                case 'fashion':
-                    await query('UPDATE articles SET title = $1, paragraph = $2, photo = $3, hide = $4, section = $5, size = $6 WHERE ID = $7', [title, paragraph, photo, hide, section, size, ID]);
-                    break;
-        
-                default:
-                    await query('UPDATE articles SET title = $1, paragraph = $2, photo = $3, hide = $4, section = $5 WHERE ID = $6', [title, paragraph, photo, hide, section, ID]);
-                    break;
-            }
-        
-            res.status(200).send({ message: "success !!!" });
-        
+            case 'beauty':
+                await query('UPDATE articles SET title = $1, paragraph = $2, photo = $3, hide = $4, section = $5, placement = $6 WHERE ID = $7', [title, paragraph, photo, hide, section, placement, ID]);
+                break;
+
+            case 'art':
+                await query('UPDATE articles SET title = $1, paragraph = $2, photo = $3, hide = $4, section = $5 WHERE ID = $6', [title, paragraph, photo, hide, section, ID]);
+                break;
+
+            case 'events':
+                await query('UPDATE articles SET title = $1, paragraph = $2, photo = $3, hide = $4, section = $5 WHERE ID = $6', [title, paragraph, photo, hide, section, ID]);
+                break;
+
+            case 'top-news':
+                await query('UPDATE articles SET title = $1, paragraph = $2, photo = $3, hide = $4, section = $5 WHERE ID = $6', [title, paragraph, photo, hide, section, ID]);
+                break;
+
+            case 'fashion':
+                await query('UPDATE articles SET title = $1, paragraph = $2, photo = $3, hide = $4, section = $5, size = $6 WHERE ID = $7', [title, paragraph, photo, hide, section, size, ID]);
+                break;
+
+            default:
+                await query('UPDATE articles SET title = $1, paragraph = $2, photo = $3, hide = $4, section = $5 WHERE ID = $6', [title, paragraph, photo, hide, section, ID]);
+                break;
+        }
+
+        const data = await query('SELECT * FROM articles WHERE ID = $1', [ID]);
+        res.json(data.rows);
+
     } catch (error) {
         console.error('Error querying database:', error);
         res.status(500).send('Internal Server Error');
